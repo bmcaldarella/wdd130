811318fd2e53561030425fc1e6f3be26fafea192
bmcaldarella <bmcaldarella@gmail.com>
2023-11-27 23:47:26 +0000

    changes

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -3,25 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('nav');
 
     menuIcon.addEventListener('click', function () {
-      // Alternar la clase 'show' para controlar la visibilidad del menú
       nav.classList.toggle('show');
     });
 
-    // Ocultar el menú cuando se hace clic en un enlace (opcional)
     nav.addEventListener('click', function () {
       nav.classList.remove('show');
     });
   });
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Obtiene los elementos que deseas animar
   const elementsToFadeIn = document.querySelectorAll('.home .img-banner, .home .hi, .home .name, .home .title-hm, .home .btn-hm');
 
-  // Añade la clase "fade-in" a cada elemento después de un breve retraso
   elementsToFadeIn.forEach((element, index) => {
     setTimeout(() => {
       element.classList.add('fade-in', 'active');
-    }, index * 200); // Ajusta el valor de retraso según sea necesario
+    }, index * 200); 
   });
 });
 
@@ -40,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
           typeWriter(text, i, fnCallback);
         }, 40); 
       } else if (typeof fnCallback == "function") {
-        setTimeout(fnCallback, 700);
+        setTimeout(fnCallback, 1500);
       }
     }
   
